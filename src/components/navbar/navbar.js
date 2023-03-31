4c8b41bbef03e381ea69f808492281509d413e59
FranciscoGontijo <franciscoacmg@gmail.com>
2023-03-31 15:16:25 +1100

    Navigation bar structure and CSS

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 import './navbar.css';
 
 const NavigationBar = () => {
-    const [navigationDisplay, setNavigationDisplay] = useState(false);
 
     return (
         <nav className="nav-bar">
-            
+            <div className="logo-container">
+                <Link className="name-logo" to="/">Francisco Gontijo</Link>
+            </div>
+            <ul className="nav-bar-link-list">
+                <li className="nav-link">
+                    <Link className="link" to="/">Home</Link>
+                </li>
+                <li className="nav-link">
+                    <Link className="link" to="/about">About</Link>
+                </li>
+            </ul>
         </nav>
     )
 };
