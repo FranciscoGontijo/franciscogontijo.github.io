8d5c09878e90fe564ff8873b85307497a344f81b
FranciscoGontijo <franciscoacmg@gmail.com>
2023-04-05 11:49:30 +1000

    Mobile screen projects section

---
+++ src/components/mobileprojects/mobileprojects.js
@@ -0,0 +1,179 @@
+import React, { useState } from "react";
+
+import "./mobileprojects.css";
+
+//import images 
+import PomodoroProjectSrc from "../../assets/images/Pomodoro Project.png";
+import PomodoroProjectChartsSrc from "../../assets/images/Pomodoro Stats.png";
+import RentalCarProjectSrc from "../../assets/images/Rental Car.png";
+import RentalCarProjectFleetSrc from "../../assets/images/RentalCar Fleet Section.png";
+
+const MobileDisplayProjects = () => {
+    const [firstProjectStatus, setFirstProjectStatus] = useState('info');
+    const [secondProjectStatus, setSecondProjectStatus] = useState('info');
+    const [thirdProjectStatus, setThirdProjectStatus] = useState('info');
+
+    const handleNextImage = (status, project) => {
+        if (project === 'first-project') {
+            if (status === 'info') {
+                setFirstProjectStatus('first-image');
+            };
+            if (status === 'first-image') {
+                setFirstProjectStatus('second-image');
+            };
+            if (status === 'second-image') {
+                setFirstProjectStatus('info');
+            };
+        };
+        if (project === 'second-project') {
+            if (status === 'info') {
+                setSecondProjectStatus('first-image');
+            };
+            if (status === 'first-image') {
+                setSecondProjectStatus('second-image');
+            };
+            if (status === 'second-image') {
+                setSecondProjectStatus('info');
+            };
+        };
+        if (project === 'third-project') {
+            if (status === 'info') {
+                setThirdProjectStatus('first-image');
+            };
+            if (status === 'first-image') {
+                setThirdProjectStatus('second-image');
+            };
+            if (status === 'second-image') {
+                setThirdProjectStatus('info');
+            };
+        };
+    };
+
+    const handleBackImage = (status, project) => {
+        if (project === 'first-project') {
+            if (status === 'info') {
+                setFirstProjectStatus('second-image');
+            };
+            if (status === 'first-image') {
+                setFirstProjectStatus('info');
+            };
+            if (status === 'second-image') {
+                setFirstProjectStatus('first-image');
+            };
+        };
+        if (project === 'second-project') {
+            if (status === 'info') {
+                setSecondProjectStatus('second-image');
+            };
+            if (status === 'first-image') {
+                setSecondProjectStatus('info');
+            };
+            if (status === 'second-image') {
+                setSecondProjectStatus('first-image');
+            };
+        };
+        if (project === 'third-project') {
+            if (status === 'info') {
+                setThirdProjectStatus('second-image');
+            };
+            if (status === 'first-image') {
+                setThirdProjectStatus('info');
+            };
+            if (status === 'second-image') {
+                setThirdProjectStatus('first-image');
+            };
+        };
+    };
+
+    return (
+        <section className="mobile-projects-container">
+            <div className="project-title">
+                <h2>Projects</h2>
+                <div className="bar"></div>
+            </div>
+            <div className="project-container">
+                {firstProjectStatus === "info" && <div className="project-info">
+                    <h3>Pomodoro project</h3>
+                    <p>This project is a responsive React application that features a Pomodoro timer with settings and
+                        statistics for the user. The application showcases the use of a range of front-end and back-end
+                        tools and libraries as React, Redux, AWS Cognito, Mongoose, Axios, Chart.js and Moment</p>
+                    <div className="view-project-div">
+                        <a href="https://quickstopwatch.netlify.app/" target="_blank"><button
+                            className="view-project-button">View Project</button></a>
+                        <a className="gitpage" href="https://github.com/FranciscoGontijo/pomodoro-project" target="_blank">
+                            <i className="fab fa-github"></i>
+                        </a>
+                    </div>
+                </div>}
+                {firstProjectStatus === "first-image" && <div className="project-img">
+                    <img src={PomodoroProjectSrc} alt="pomodoro timer" />
+                </div>}
+                {firstProjectStatus === "second-image" && <div className="project-img">
+                    <img src={PomodoroProjectChartsSrc} alt="pomodoro charts" />
+                </div>}
+                <div className="tracking-balls">
+                    <div className={firstProjectStatus === "info" ? "track-ball active-ball" : "track-ball"}></div>
+                    <div className={firstProjectStatus === "first-image" ? "track-ball active-ball" : "track-ball"}></div>
+                    <div className={firstProjectStatus === "second-image" ? "track-ball active-ball" : "track-ball"}></div>
+                </div>
+                <button className="go-back-image-button" onClick={() => handleBackImage(firstProjectStatus, 'first-project')} value={firstProjectStatus}>Image</button>
+                <button className="go-next-image-button" onClick={() => handleNextImage(firstProjectStatus, 'first-project')}>Image</button>
+            </div>
+            <div className="project-container">
+                <div className="tracking-balls">
+                    <div className={secondProjectStatus === "info" ? "track-ball active-ball" : "track-ball"}></div>
+                    <div className={secondProjectStatus === "first-image" ? "track-ball active-ball" : "track-ball"}></div>
+                    <div className={secondProjectStatus === "second-image" ? "track-ball active-ball" : "track-ball"}></div>
+                </div>
+                {secondProjectStatus === 'info' && <div className="project-info">
+                    <h3>Rental Car SPA</h3>
+                    <p>This is a full responsive React single-page application (SPA) for a rental car website. Focused
+                        only on the front-end part of the website.</p>
+                    <div className="view-project-div">
+                        <a href="#" target="_blank" rel="noreferrer"><button className="view-project-button">View Project</button></a>
+                        <a className="gitpage" href="https://github.com/FranciscoGontijo/Rental-car-website"
+                            target="_blank" rel="noreferrer">
+                            <i className="fab fa-github"></i>
+                        </a>
+                    </div>
+                </div>}
+                {secondProjectStatus === "first-image" && <div className="project-img">
+                    <img src={RentalCarProjectSrc} alt="Rental car home page" />
+                </div>}
+                {secondProjectStatus === "second-image" && <div className="project-img">
+                    <img src={RentalCarProjectFleetSrc} alt="Rental car home page fleet section" />
+                </div>}
+                <button className="go-back-image-button" onClick={() => handleBackImage(secondProjectStatus, 'second-project')}>Image</button>
+                <button className="go-next-image-button" onClick={() => handleNextImage(secondProjectStatus, 'second-project')}>Image</button>
+            </div>
+            <div className="project-container">
+                <div className="tracking-balls">
+                    <div className={thirdProjectStatus === "info" ? "track-ball active-ball" : "track-ball"}></div>
+                    <div className={thirdProjectStatus === "first-image" ? "track-ball active-ball" : "track-ball"}></div>
+                    <div className={thirdProjectStatus === "second-image" ? "track-ball active-ball" : "track-ball"}></div>
+                </div>
+                {thirdProjectStatus === 'info' && <div className="project-info">
+                    <h3>Project name</h3>
+                    <p>Project description</p>
+                    <div className="view-project-div">
+                        <a href="#" target="_blank" rel="noreferrer"><button className="view-project-button">View Project</button></a>
+                        <a className="gitpage" href="https://github.com/FranciscoGontijo/Rental-car-website"
+                            target="_blank" rel="noreferrer">
+                            <i className="fab fa-github"></i>
+                        </a>
+                    </div>
+                </div>}
+                {thirdProjectStatus === "first-image" && <div className="project-img">
+                    <img src={PomodoroProjectSrc} alt="project" />
+                </div>}
+                {thirdProjectStatus === "second-image" && <div className="project-img">
+                    <img src={RentalCarProjectSrc} alt="project" />
+                </div>}
+                <button className="go-back-image-button" onClick={() => handleBackImage(thirdProjectStatus, 'third-project')}>Image</button>
+                <button className="go-next-image-button" onClick={() => handleNextImage(thirdProjectStatus, 'third-project')}>Image</button>
+            </div>
+        </section>
+    )
+};
+
+export default MobileDisplayProjects;
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+//import Util functions 
+import useWindowSize from "../util/useWindowSize";
 
 //import components
-import Projects from "../components/projects/projects"
+import HeroBannerMobile from "../components/mobileherobanner/mobileherobanner";
+import MobileDisplayProjects from "../components/mobileprojects/mobileprojects";
+import HeroBanner from "../components/herobanner/herobanner";
+import Projects from "../components/projects/projects";
+
 
 import "./homepage.css";
 
+
 const HomePage = () => {
+    const screenSize = useWindowSize();
+    const [display, setDisplay] = useState('mobile');
+
+    useEffect(() => {
+        if (screenSize.width > 800) {
+          setDisplay('desktop');
+        }
+        if (screenSize.width <= 800) {
+          setDisplay('mobile');
+        }
+      }, [screenSize]);
+
     return (
         <div>
-            <h1>Home</h1>
-            <Projects />
+            {display === 'mobile' && <HeroBannerMobile />}
+            {display === 'mobile' && <MobileDisplayProjects />}
+            {display === 'desktop' && <HeroBanner />}
+            {display === 'desktop' && <Projects />}
         </div>
     )
 };
 
 export default HomePage;
+    
