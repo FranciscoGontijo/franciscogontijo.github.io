87df50cdc337ef67f2fec2e9dbadd2a7711f1813
FranciscoGontijo <franciscoacmg@gmail.com>
2024-01-22 14:25:26 -0300

    Update about page with new skills

--- src/pages/aboutpage.js
+++ src/pages/aboutpage.js
@@ -67,6 +67,8 @@ const AboutPage = () => {
                                 <p>NodeJS</p>
                                 <p>Express</p>
                                 <p>Mongoose</p>
+                                <p>Web Sokets</p>
+                                <p>JWT authentication</p>
                             </div>
                         </div>
                     </div>
@@ -77,55 +79,57 @@ const AboutPage = () => {
             }
             {display === 'desktop' &&
                 <section className="big-screen-about-page">
-                <div className="intro-banner">
-                    <h2>Front-End Developer</h2>
-                    <p>Creating Engaging and User-Friendly Web Experiences</p>
-                </div>
-                <div className="profile-picture-container">
-                    <img src={ProfilePictureSrc} className="about-page-profile-picture" alt="Francisco's profile" />
-                </div>
-                <div className="background-wallpaper">
-                    <div className="about-text">
-                        <h3>Hi, I'm Francisco. Nice to meet you.</h3>
-                        <p>As a Front-End Developer, I bring a strong foundation in HTML, CSS,
-                            JavaScript, and TypeScript to every project.
-                            With proficiency in React and experience in responsive design,
-                            I have the skills to create websites and applications that are both visually appealing and functional.
-                            Additionally, I am experienced in API integration and REST API design,
-                            enabling me to seamlessly integrate back-end systems with front-end user interfaces.
-                            Let's collaborate to build something great together.</p>
+                    <div className="intro-banner">
+                        <h2>Front-End Developer</h2>
+                        <p>Creating Engaging and User-Friendly Web Experiences</p>
                     </div>
-                    <div className="tools-and-languages-container">
-                        <div>
-                            <i></i>
+                    <div className="profile-picture-container">
+                        <img src={ProfilePictureSrc} className="about-page-profile-picture" alt="Francisco's profile" />
+                    </div>
+                    <div className="background-wallpaper">
+                        <div className="about-text">
+                            <h3>Hi, I'm Francisco. Nice to meet you.</h3>
+                            <p>As a Front-End Developer, I bring a strong foundation in HTML, CSS,
+                                JavaScript, and TypeScript to every project.
+                                With proficiency in React and experience in responsive design,
+                                I have the skills to create websites and applications that are both visually appealing and functional.
+                                Additionally, I am experienced in API integration and REST API design,
+                                enabling me to seamlessly integrate back-end systems with front-end user interfaces.
+                                Let's collaborate to build something great together.</p>
                         </div>
-                        <h3>Web Developer</h3>
-                        <p>I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
-                        <div className="languages-container">
-                            <p className="p-title">Languages: </p>
-                            <p>HTML, CSS, JavaScript and TypeScript</p>
-                        </div>
-                        <div className="tools-container">
-                            <p className="p-title">Dev Tools: </p>
-                            <p>Next.JS</p>
-                            <p>React</p>
-                            <p>React Native</p>
-                            <p>React router</p>
-                            <p>Redux</p>
-                            <p>React context</p>
-                            <p>Axios</p>
-                            <p>Jest</p>
-                            <p>AWS Cognito</p>
-                            <p>NodeJS</p>
-                            <p>Express</p>
-                            <p>Mongoose</p>
+                        <div className="tools-and-languages-container">
+                            <div>
+                                <i></i>
+                            </div>
+                            <h3>Web Developer</h3>
+                            <p>I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
+                            <div className="languages-container">
+                                <p className="p-title">Languages: </p>
+                                <p>HTML, CSS, JavaScript and TypeScript</p>
+                            </div>
+                            <div className="tools-container">
+                                <p className="p-title">Dev Tools: </p>
+                                <p>Next.JS</p>
+                                <p>React</p>
+                                <p>React Native</p>
+                                <p>React router</p>
+                                <p>Redux</p>
+                                <p>React context</p>
+                                <p>Axios</p>
+                                <p>Jest</p>
+                                <p>AWS Cognito</p>
+                                <p>NodeJS</p>
+                                <p>Express</p>
+                                <p>Mongoose</p>
+                                <p>Web Sokets</p>
+                                <p>JWT authentication</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="about-page-contact-section">
-                    <button className="hi-button"><Link className="link" to="/contact">Say Hello!!</Link></button>
-                </div>
-            </section>
+                    <div className="about-page-contact-section">
+                        <button className="hi-button"><Link className="link" to="/contact">Say Hello!!</Link></button>
+                    </div>
+                </section>
             }
         </section>
     )
